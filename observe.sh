88be998d97170d348e23800332841fbfe0a1541f
Ravi Kumar P <ravikup@users.noreply.github.com>
2019-12-18 12:53:52 +0530

    Fix JMX listening on localhost

--- observe.sh
+++ observe.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-CONNECTION="service:jmx:rmi:///jndi/rmi://0.0.0.0:12341/jmxrmi"
+CONNECTION="service:jmx:rmi:///jndi/rmi://localhost:12341/jmxrmi"
 
 jconsole -interval=1 $CONNECTION
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-JAVAOPTS="-Dcom.sun.management.jmxremote.port=12341 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+JAVAOPTS="-Dcom.sun.management.jmxremote.port=12341 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=localhost"
 
 java -jar -Xms$1 -Xmx$2 $JAVAOPTS ./build/libs/setmembership-0.1.0.jar $3 $4
